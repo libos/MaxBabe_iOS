d1d65c047d7b835fbcbd4256ca2f1107d7d21006
LiberLiu <llb0536@gmail.com>
2015-05-26 12:37:54 +0800

    Initial Commit

---
+++ MaxBabeTests/MaxBabeTests.swift
@@ -0,0 +1,36 @@
+//
+//  MaxBabeTests.swift
+//  MaxBabeTests
+//
+//  Created by Liber on 5/26/15.
+//  Copyright (c) 2015 Maxtain. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MaxBabeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
